1ffec83c80e84bf4ce54c0ce5e4b4f7218074e5f
hunter87 <83773586+Hunter87ff@users.noreply.github.com>
2023-10-19 12:09:14 +0530

    Update main.py

--- src/main.py
+++ src/main.py
@@ -12,25 +12,22 @@ def upload():
   <input type="file" name="file">
   <input type="submit" value="Upload">
 </form>
-    """
+    """    #it'll run, if the above return isn't working properly
 
 
 
 @app.route('/fsend', methods=['POST'])
 def upload_file():
-    data = request.form.to_dict()
-    print(data)
-    file = request.files['file']
-
-    fn = os.path.join("D:/github/FileShare/src/files/", file.filename)
-    print(fn)
+    # data = request.form.to_dict()
+    # print(data)
+    file = request.files['file']  #this is the file
+    fn = os.path.join("D:/github/FileShare/src/files/", file.filename)  #change the file path to your file path
     try:
         with open(fn, "wb") as f:
-            f.write(file.read())
+            f.write(file.read())   #write and save the file
     except IOError as e:
-        return f"Error saving file: {e}"
-    return "<script>alert('uploaded'); window.location.href='/'</script>"
-    return "<script>alert('uploaded')</script>"
+        return "<script>alert('Unable to upload file, Try again!!'); window.location.href='/'</script>"  #error handelling for file saving
+    return "<script>alert('uploaded'); window.location.href='/'</script>"  
 
-#os.system("start chrome http://localhost:8787")
-app.run(host="0.0.0.0", port=8787, debug=True)
+#os.system("start chrome http://localhost:8787")  
+app.run(host="0.0.0.0", port=8787, debug=True)  #disable the debugger if you want
