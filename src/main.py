55e44d3496d80476861b65c958807875ac1c39f6
hunter87 <83773586+Hunter87ff@users.noreply.github.com>
2023-12-24 12:14:55 +0530

    Update main.py

--- src/main.py
+++ src/main.py
@@ -29,5 +29,5 @@ def upload_file():
         return "<script>alert('Unable to upload file, Try again!!'); window.location.href='/'</script>"  #error handelling for file saving
     return "<script>alert('uploaded'); window.location.href='/'</script>"  
 
-os.system("start chrome http://localhost:8080")  
+#os.system("start chrome http://localhost:8080")  
 app.run(host="0.0.0.0", port=8080, debug=True)  #disable the debugger if you want
