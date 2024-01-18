0e9659eae5688fa6390fbd49b20a7a788e422709
hunter87ff <huntergaming702@gmail.com>
2024-01-18 13:17:56 +0530

    added gui support

--- src/main.py
+++ src/main.py
@@ -2,17 +2,29 @@
 # Github : https://github.com/hunter87ff
 # Insta  : https://instagram.com/im_hunter87
 # Youtube: https://youtube.com/@hunter87
-
-try:
-    import flask, socket, os, qrcode
-    from flask import Flask, request, render_template
-except:
-    os.system("pip install -r requirements.txt")
 lgr='\033[1;32m' 
 red='\033[0;31m' 
 cyan='\033[36m' 
 blue='\033[34m'
 NC='\033[0m'
+
+
+try:
+    import flask, socket, os, qrcode, webview,threading
+    from flask import Flask, request, render_template, redirect
+except:
+    os.system("pip install -r requirements.txt")
+
+
+cur_dir = os.path.dirname(__file__)
+os.chdir(cur_dir) # change working directory
+
+#print(os.system("dir")) #to see the current working directory, it should be the path to the file
+
+for i in os.listdir("static/img"):
+    print(i)
+
+
 
 def local_ip():
     try:
@@ -24,32 +36,41 @@ def local_ip():
         print(f"Error getting local IP address: {e}")
         return None
 
-app = Flask("FileShare")
-app.static_folder = "static"
+app = Flask("FileShare", static_folder = "static")
 
-@app.route("/")
+@app.errorhandler(404)
+def page_not_found(e):
+    return redirect("/upload")
+    return render_template('form.html'), 404
+
+@app.route("/upload")
 def upload():
     try:return render_template("form.html")
     except:return """
-    <form action="/fsend" method="POST" enctype="multipart/form-data">
-  <input type="file" name="file">
-  <input type="submit" value="Upload">
-</form>
-    """    #it'll run, if the form template isn't working properly
+                    <form action="/fsend" method="POST" enctype="multipart/form-data">
+                    <input type="file" name="file">
+                    <input type="submit" value="Upload">
+                    </form>
+    """
+
+
+@app.route("/")
+def qrimg():
+        return render_template("qrcode.html")
 
 
 
 @app.route('/fsend', methods=['POST'])
 def upload_file():
-    # data = request.form.to_dict()
     file = request.files['file']
     fn = ""
     if "win" in os.sys.platform:
-        fn = os.path.join(os.environ["USERPROFILE"]+r"\Downloads", file.filename)  #file path config is configured for windows. change it based on your os.
+        fn = os.path.join(os.environ["USERPROFILE"]+r"\Downloads", file.filename)
     else: fn = file.filename
     try:
         with open(fn, "wb") as f:
-            f.write(file.read())   #write and save the file
+            f.write(file.read())
+            print(f"File saved as {fn}")
     except IOError as e:
         return "<script>alert('Unable to upload file, Try again!!'); window.location.href='/'</script>"  #error handelling
     return f"<script>alert('uploaded'); window.location.href='/'</script>"  
@@ -58,15 +79,19 @@ url = f"{lgr}URL to send file : {red}http://{local_ip()}:8787{NC}"
 if "win" in os.sys.platform:
     url = f"URL to send file : http://{local_ip()}:8787"
     qr = qrcode.QRCode(version=1,error_correction=qrcode.constants.ERROR_CORRECT_L,box_size=10,border=4,)
-    data = f"{local_ip()}:8787"
+    data = f"{local_ip()}:8787/upload"
     qr.add_data(data)
     qr.make(fit=True)
     img = qr.make_image(fill_color="black", back_color="white")
-    img.save("url_qr.png")
-    img.show()
-    os.system("del url_qr.png")
+    img.save(r"static/url_qr.png")
+    #img.show()
+    #os.system(r'del "static\url_qr.png"')
 
 
-print(url)
-if __name__ == '__main__':
+def loop1():
     app.run(host="0.0.0.0", port=8787)
+
+thread1 = threading.Thread(target=loop1)
+thread1.start()
+webview.create_window('FileShare', app)
+webview.start(debug=False)
